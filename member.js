a0c8fdde606cae2f97343a3e7b85f557199b639e
Goedies04 <79329044+Goedies04@users.noreply.github.com>
2024-01-11 11:23:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/member.html',
+        scope: {
+            info: '='
+        },
+        controller: function ($scope) {
+            $scope.member = {};
+            $scope.member.name = $scope.info.name;
+            $scope.member.skills = $scope.info.skills;
+        }
+    };
+}
